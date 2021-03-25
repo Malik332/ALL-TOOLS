f078ddf880a0c9ab5a361c4cba6b7383dd82ba90
Malik Atiquz zaman <71874818+Malik332@users.noreply.github.com>
2021-03-25 19:11:56 +0530

    Create squareroot.py

---
+++ squareroot.py
@@ -0,0 +1,10 @@
+# python program to calculate squareroot
+# By Malik Atiquz zaman
+# Instagram : malik_atiquz_.zaman
+# To take the input from usr
+num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+print("Nice work")
